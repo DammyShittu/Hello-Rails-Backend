ede44502bddfa845e0bd30ffd99a651f2566e023
Adedamola Shittu <shittuadedamola@rocketmail.com>
2022-01-11 11:57:20 +0100

    Add Data To the Seed File To Populate Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+greeting_one = Message.create(greeting: "Hello World, I am here!")
+greeting_two = Message.create(greeting: "Hello Rails")
+greeting_three = Message.create(greeting: "Hey everyone, today is Tuesday!")
+greeting_four = Message.create(greeting: "Hello Micronauts, happy coding")
+greeting_five = Message.create(greeting: "Hello Facebook, React is cool")
