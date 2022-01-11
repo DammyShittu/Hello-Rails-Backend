7ee9800cd5d0f82075b37ce62aa0ffd4887de07a
Adedamola Shittu <shittuadedamola@rocketmail.com>
2022-01-11 13:00:47 +0100

    Generate Random Message And Configure Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :messages,
+            only: [:index],
+            defaults: { format: :json }
 end
